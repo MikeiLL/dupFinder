f6310a2c82e3269bfe4f51e83bfb14cb5b7edb85
Mike iLL Kilmer <mike@mzoo.org>
2015-06-14 11:35:59 -0500

    Remove unnecessary close statement.

--- dupFinder.py
+++ dupFinder.py
@@ -53,7 +53,6 @@ def hashfile(path, blocksize = 65536):
         while len(buf) > 0:
             hasher.update(buf)
             buf = afile.read(blocksize)
-        afile.close()
     return hasher.hexdigest()
  
  
